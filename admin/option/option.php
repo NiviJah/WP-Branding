fcb26260855dfbafd4fa0f51c719991d65f2e71b
NiviJah <nivijah@gmail.com>
2014-10-20 11:34:40 +0300

    Complete before add ons

---
+++ admin/option/option.php
@@ -0,0 +1,151 @@
+<?php
+$file = dirname(dirname(dirname(__FILE__))) . '/index.php.php';
+$plugin_url = plugin_dir_url($file);
+// Output something like: http://example.com/wp-content/plugins/your-plugin/
+$plugin_path = plugin_dir_path($file);
+// Output something like: /home/mysite/www/wp-content/plugins/your-plugin/
+
+return array(
+	'title' => __('Agent Branding Option Panel', 'vp_textdomain'),
+	'logo' => $plugin_url . 'img/logo.png',
+	'menus' => array(
+		array(
+			'title' => __('Login', 'vp_textdomain'),
+			'name' => 'submenu_1',
+			'icon' => 'font-awesome:fa-unlock',
+			'controls' => array(
+				array(
+					'type' => 'section',
+					'title' => __('Login Section', 'vp_textdomain'),
+					'name' => 'section_1',
+					'description' => __('Tweak the look and feel of the login screen', 'vp_textdomain'),
+					'fields' => array(
+						array(
+							'type' => 'upload',
+							'name' => 'logo_upload',
+							'label' => __('Upload', 'vp_textdomain'),
+							'description' => __('Upload a logo to use in the login screen, size should be 330px/115px', 'vp_textdomain'),
+							'default' => $plugin_url . 'img/logo.png',
+							),
+						array(
+							'type' => 'textbox',
+							'name' => 'logo_url',
+							'label' => __('Logo URL', 'vp_textdomain'),
+							'description' => __('Link of the logo in login screen', 'vp_textdomain'),
+							'default' => 'http://NiviJah.com',
+							),
+						array(
+							'type' => 'textbox',
+							'name' => 'logo_url_alt',
+							'label' => __('Logo Alt', 'vp_textdomain'),
+							'description' => __('Alt text of the logo image', 'vp_textdomain'),
+							'default' => 'Awesome Site Made By Awesome People',
+							),
+						array(
+							'type' => 'upload',
+							'name' => 'bg_login',
+							'label' => __('Upload', 'vp_textdomain'),
+							'description' => __('Upload a background to use in the login screen, best would be to use repeated one', 'vp_textdomain'),
+							'default' => $plugin_url . 'img/skulls.png',
+							),
+
+						),
+),
+
+),
+),
+array(
+	'title' => __('Admin Tweaks', 'vp_textdomain'),
+	'name' => 'submenu_2',
+	'icon' => 'font-awesome:fa-bar-chart-o',
+	'controls' => array(
+		array(
+			'type' => 'section',
+			'title' => __('Admin Section', 'vp_textdomain'),
+			'name' => 'section_2',
+			'description' => __('Admin tweaks', 'vp_textdomain'),
+			'fields' => array(
+				array(
+					'type' => 'wpeditor',
+					'name' => 'admin_footer_text',
+					'label' => __('Admin Footer Text', 'vp_textdomain'),
+					'description' => __('Replace the default wordpress text', 'vp_textdomain'),
+					'use_external_plugins' => '0',
+					'default' => 'Footer Text <a href="http://www.google.com"> And HTML </a>',
+					),
+				array(
+					'type' => 'textbox',
+					'name' => 'meta_box_title',
+					'label' => __('Title for custom Meta Box', 'vp_textdomain'),
+					'description' => __('Add a title for the custom metabox', 'vp_textdomain'),
+					'default' => 'Welcome',
+					),
+				array(
+					'type' => 'wpeditor',
+					'name' => 'custom_meta_box',
+					'label' => __('Custom Meta Box', 'vp_textdomain'),
+					'description' => __('Add a custom metabox to the dashboard', 'vp_textdomain'),
+					'use_external_plugins' => '1',
+					'default' => '',
+					),
+				array(
+					'type' => 'checkbox',
+					'name' => 'remove_meta',
+					'label' => __('Remove Admin Metaboxes', 'vp_textdomain'),
+					'description' => __('Do you want to remove the dashboard metaboxes ?'),
+					'items' => array(
+						array(
+							'value' => 'remove',
+							'label' => __('Yes', 'vp_textdomain'),
+							),
+						),
+					),
+
+
+
+				),
+),
+),
+),
+
+array(
+	'title' => __('Color Tweaks', 'vp_textdomain'),
+	'name' => 'submenu_3',
+	'icon' => 'font-awesome:fa-star-half-o',
+	'controls' => array(
+		array(
+			'type' => 'section',
+			'title' => __('Color Tweaks', 'vp_textdomain'),
+			'name' => 'section_3',
+			'description' => __('Colors all around', 'vp_textdomain'),
+			'fields' => array(
+
+    array(
+        'type' => 'color',
+        'name' => 'menu_bg',
+        'label' => __('Menu Background', 'vp_textdomain'),
+        'description' => __('Main background color for the admin menu items', 'vp_textdomain'),
+        'default' => '#3FB14D',
+        'format' => 'hex',
+    ),
+        array(
+        'type' => 'color',
+        'name' => 'text_color',
+        'label' => __('Hover Text Color', 'vp_textdomain'),
+        'description' => __('Main color for the text in menu items', 'vp_textdomain'),
+        'default' => '#3FB14D',
+        'format' => 'hex',
+    ),
+
+				),
+),
+),
+),
+
+),
+
+);
+
+/**
+ *EOF
+ */
--- index.php
+++ index.php
@@ -9,101 +9,206 @@
  * License: GPL2
  */
 
-// Include Vafpress Framework
-require_once 'includes/bootstrap.php';
+$plugin_path = ABSPATH. 'wp-content/plugins/agent_plugin/';
 
- // DEFINE DEFAULT PATH
-$plugin_dir_path = dirname(__FILE__);
+//Debug
+add_action('activated_plugin','save_error');
+function save_error(){
+  file_put_contents($plugin_path . '/error.html', ob_get_contents());
+}
+
+//--------------------------------------------------------
+
+/**
+ * Include Vafpress Framework
+ */
+require_once $plugin_path . 'vafpress-framework/bootstrap.php';
+
+
+// options
+$tmpl_opt  = $plugin_path . '/admin/option/option.php';
+
+/**
+ * Create instance of Options
+ */
+$theme_options = new VP_Option(array(
+    'is_dev_mode'           => false,                                  // dev mode, default to false
+    'option_key'            => 'agnet_branding',                           // options key in db, required
+    'page_slug'             => 'agnet_branding',                           // options page slug, required
+    'template'              => $tmpl_opt,                              // template file path or array, required
+    'menu_page'             => 'options-general.php',                           // parent menu slug or supply `array` (can contains 'icon_url' & 'position') for top level menu
+    'use_auto_group_naming' => true,                                   // default to true
+    'use_util_menu'         => true,                                   // default to true, shows utility menu
+    'minimum_role'          => 'edit_theme_options',                   // default to 'edit_theme_options'
+    'layout'                => 'fixed',                                // fluid or fixed, default to fixed
+    'page_title'            => __( 'Agent Branding', 'vp_textdomain' ), // page title
+    'menu_label'            => __( 'Agent Branding', 'vp_textdomain' ), // menu label
+    ));
+
+
+//-------------------------------------------------------
 
 // ADD STYLESHEET TO LOGIN PAGE
 function agenti_my_login_stylesheet() {
 
     wp_enqueue_style( 'custom-login', plugins_url('css/login-styles.css', __FILE__) );
 
-    }
-    add_action( 'login_enqueue_scripts', 'agenti_my_login_stylesheet' );
-
-// CHANGE THE LOGO
-function agenti_my_login_logo() { ?>
-    <style type="text/css">
-        body.login div#login h1 a {
-            background-image: url(<?php echo plugins_url('img/logo.png', __FILE__); ?>);
-
-            background-size: 320px 115px;
-        }
-    </style>
-<?php }
-    add_action( 'login_enqueue_scripts', 'agenti_my_login_logo' );
-
-// CHANGE URL OF LOGO
-function agenti_my_login_logo_url() {
-    return 'http://agent.co.il';
-    }
-    add_filter( 'login_headerurl', 'agenti_my_login_logo_url' );
-
-// CHANGE LOGO IMG ALT
-function agenti_my_login_logo_url_title() {
-    return 'A Site By Agent Interactive';
 }
-    add_filter( 'login_headertitle', 'agenti_my_login_logo_url_title' );
+add_action( 'login_enqueue_scripts', 'agenti_my_login_stylesheet' );
+
 
 // ADD CUSTOM STYLESHEET TO ADMIN AREA
 function agenti_customAdmin() {
-  wp_enqueue_style( 'custom-login', plugins_url('css/agent-admin-style.css', __FILE__) );
+    ?>
+    <!-- wp_enqueue_style( 'custom-login', plugins_url('css/agent-admin-style.css', __FILE__) ); -->
+    <?php
+
+    $menu_bg = vp_option('agnet_branding.menu_bg');
+    $text_color = vp_option('agnet_branding.text_color');
+
+    ?>
+
+    <style type="text/css">
+    
+    #adminmenu .wp-has-current-submenu .wp-submenu .wp-submenu-head, #adminmenu .wp-menu-arrow, #adminmenu .wp-menu-arrow div, #adminmenu li.current a.menu-top, #adminmenu li.wp-has-current-submenu a.wp-has-current-submenu, .folded #adminmenu li.current.menu-top, .folded #adminmenu li.wp-has-current-submenu {
+        background: <?php echo $menu_bg; ?>;
+        color: #fff;
+    }
+
+    #adminmenu .wp-submenu a:focus, #adminmenu .wp-submenu a:hover, #adminmenu a:hover, #adminmenu li.menu-top>a:focus{
+        color: <?php echo $text_color; ?> ;
+    }
+    #adminmenu li:hover div.wp-menu-image:before {
+        color: <?php echo $text_color; ?> ;
+    }
+    #wpadminbar .ab-top-menu>li.hover>.ab-item, #wpadminbar .ab-top-menu>li:hover>.ab-item, #wpadminbar .ab-top-menu>li>.ab-item:focus, #wpadminbar.nojq .quicklinks .ab-top-menu>li>.ab-item:focus{
+        background: #333;
+        color: <?php echo $text_color; ?>;
 
     }
+    #wpadminbar>#wp-toolbar a:focus span.ab-label, #wpadminbar>#wp-toolbar li.hover span.ab-label, #wpadminbar>#wp-toolbar li:hover span.ab-label {
+        color: <?php echo $text_color; ?>;
+    }
+    #wpadminbar .quicklinks .menupop ul li a:focus, #wpadminbar .quicklinks .menupop ul li a:focus strong, #wpadminbar .quicklinks .menupop ul li a:hover, #wpadminbar .quicklinks .menupop ul li a:hover strong, #wpadminbar .quicklinks .menupop.hover ul li a:focus, #wpadminbar .quicklinks .menupop.hover ul li a:hover, #wpadminbar li .ab-item:focus:before, #wpadminbar li a:focus .ab-icon:before, #wpadminbar li.hover .ab-icon:before, #wpadminbar li.hover .ab-item:before, #wpadminbar li:hover #adminbarsearch:before, #wpadminbar li:hover .ab-icon:before, #wpadminbar li:hover .ab-item:before, #wpadminbar.nojs .quicklinks .menupop:hover ul li a:focus, #wpadminbar.nojs .quicklinks .menupop:hover ul li a:hover {
+        color: <?php echo $text_color; ?>;
+    }
+    .dash-logo{
+        display: block;
+        width: 90%;
+    }
+    </style>
+
+    <?php }
     add_action('admin_head', 'agenti_customAdmin');
 
+// CHANGE THE LOGO
+    function agenti_my_login_logo() { 
+        $image = vp_option('agnet_branding.logo_upload');
+
+        ?>
+
+        <style type="text/css">
+        body.login div#login h1 a {
+            background-image: url( <?php echo $image; ?> );
+            background-size: 320px 115px;
+        }
+        </style>
+        <?php }
+        add_action( 'login_enqueue_scripts', 'agenti_my_login_logo' );
+
+// Background Image
+        function agenti_bg_login() { 
+            $bg = vp_option('agnet_branding.bg_login');
+            ?>
+            <style type="text/css">
+            body.login {
+                background-image: url(<?php echo $bg; ?>);
+            }
+            </style>
+            <?php }
+            add_action( 'login_enqueue_scripts', 'agenti_bg_login' );
+
+// CHANGE URL OF LOGO
+            function agenti_my_login_logo_url() {
+                $logo_url = vp_option('agnet_branding.logo_url');
+                return $logo_url;
+            }
+            add_filter( 'login_headerurl', 'agenti_my_login_logo_url' );
+
+// CHANGE LOGO IMG ALT
+            function agenti_my_login_logo_url_title() {
+                $logo_alt = vp_option('agnet_branding.logo_url_alt');
+                return $logo_alt;
+            }
+            add_filter( 'login_headertitle', 'agenti_my_login_logo_url_title' );
+
+
+
 //CHANGE THE DEFAULT FOOTER TEXT IN ADMIN
-function agenti__admin_footer_text () {
-    echo 'Another Website Made with Love in <a href="http://agent.co.il">Agent</a>.';
-    }
-    add_filter( 'admin_footer_text', 'agenti__admin_footer_text' );
+            function agenti__admin_footer_text () {
+                $admin_footer_text = vp_option('agnet_branding.admin_footer_text');
+        // echo 'Another Website Made with Love in <a href="http://agent.co.il">Agent</a>.';
+                echo $admin_footer_text;
+            }
+            add_filter( 'admin_footer_text', 'agenti__admin_footer_text' );
 
 
-// remove unwanted dashboard widgets for relevant users
-function agenti_remove_dashboard_widgets() {
+            $remove_metaboxes = vp_option('agnet_branding.remove_meta');
+            if ($remove_metaboxes == true) {
 
+                function tidy_dashboard()  
+                {
+                  global $wp_meta_boxes, $current_user;
 
-    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
-    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
-    remove_meta_box( 'icl_dashboard_widget', 'dashboard', 'normal');//since 3.8
+  // remove incoming links info for authors or editors
+                  if(in_array('author', $current_user->roles) || in_array('editor', $current_user->roles))
+                  {
+                    unset($wp_meta_boxes['dashboard']['normal ']['core']['dashboard_incoming_links']);
+                }
 
-    }
-    add_action( 'wp_dashboard_setup', 'agenti_remove_dashboard_widgets' );
+//Right Now - Comments, Posts, Pages at a glance
+                unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);  
+//Recent Comments
+                unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);  
+//Incoming Links
+                unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);  
+//Plugins - Popular, New and Recently updated Wordpress Plugins
+                unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+                unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
+                unset($wp_meta_boxes['dashboard']['normal']['core']['icl_dashboard_widget']);
+//Wordpress Development Blog Feed
+                unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);  
+//Other Wordpress News Feed
+                unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);  
+//Quick Press Form
+                unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);  
+//Recent Drafts List
+                unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);  
 
+            }
+        }
+//add our function to the dashboard setup hook
+        add_action('wp_dashboard_setup', 'tidy_dashboard'); 
 
 
 // add new dashboard widgets
-    function agenti_add_dashboard_widgets() {
-        wp_add_dashboard_widget( 'agenti_dashboard_welcome', 'Welcome', 'agenti_add_welcome_widget' );
+        function agenti_add_dashboard_widgets() {
+            $custom_title = vp_option('agnet_branding.meta_box_title');
+            wp_add_dashboard_widget( 'agenti_dashboard_welcome', $custom_title, 'agenti_add_welcome_widget' );
 
-    }
+        }
 
 // ADD CONTENT TO THE NEW DASHBOAD WIDGET
-    function agenti_add_welcome_widget(){ ?>
+        function agenti_add_welcome_widget(){ 
 
-    <img src="<?php echo plugins_url('img/dash.png', __FILE__); ?>" class="dash-logo" alt=""><br />
-    <h2>Welcome To Your Website ! </h2><br />
-    <h2>Please Don't hesitate to call us if you need support.</h2>
+            $custom_meta_box = vp_option('agnet_branding.custom_meta_box');
 
-    <ul>
-        <li><a href="mailto:niv.agent@gmail.com">Send us an Email</a></li>
-        <li>Call Us: 052-4699933</li>
-        <li><a href="http://www.agent.co.il">Our Website</a></li>
-    </ul>
+            echo $custom_meta_box; 
+        }
 
-    <?php }
 
-    add_action( 'wp_dashboard_setup', 'agenti_add_dashboard_widgets' );
+        add_action( 'wp_dashboard_setup', 'agenti_add_dashboard_widgets' );
 
-// ADD TEXT TO BOTTOM OF ALL FRONTEND PAGES - NEED TESTING
-    function agent_footer() {
-        echo '<p>Made with love by <a href="http://www.agent.co.il/" target="_blank">@gent</a></p>';
-    }
-        add_action('wp_footer', 'agent_footer');
+
+
+
